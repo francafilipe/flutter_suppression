f0da52f957a14327ddf87df6405b1a889b711fdb
francafilipe <filipeffranc@gmail.com>
2021-07-30 20:03:47 -0300

    Setup simulation/integral solver - the simulation is running but it's not correct

--- 3DOF.py
+++ 3DOF.py
@@ -1,9 +1,9 @@
 # Importing dependencies
 from numpy.linalg.linalg import inv
-from scipy import *
+from scipy.integrate import odeint 
 from numpy import *
 
-def model_nocontrol(t,x,k):
+def model_nocontrol(x,t,k):
     # Importing the model's parameters
     import parameters as params
     import aerodynamics
@@ -27,13 +27,18 @@ def model_nocontrol(t,x,k):
         state[(2+nlag)*n:(3+nlag)*n,params.nDOF:2*params.nDOF] = identity(params.nDOF)                                  # second column (identities starting after second row)
         state[(2+nlag)*n:(3+nlag)*n,(2+nlag)*n:(3+nlag)*n] = -(params.Voo/params.b)*params.gamma[nlag]*identity(3)      # lag parameters
 
-    return None
+    return dot(state,x)
+
 
 from math import pi
 
-t = array([0, 10])
-x = array([0, 0, 0, 0, 0, 0])
+# Define time range and initial conditions
+t = arange(0,11,1)
+x = zeros(18)
+x[1] = 0.1
 
 k = array([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0])
 
-test = model_nocontrol(t,x,k)
+# Run integral solver
+y = odeint(model_nocontrol,x,t,args=(k,))
+print(y)
