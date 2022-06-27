0f449bd8a3a33b737ea8fe1068507e4f77eca15f
Filipe <filipeffranc@gmail.com>
2022-06-27 19:35:35 -0300

    Changes in sim conditions

--- sim3DOF.m
+++ sim3DOF.m
@@ -10,10 +10,10 @@ rho = 0.000044256;          % [lb/in3]  air density
 x0  = [5.0 0.0 0.0];        % Initial Condition {h theta beta}
 
 dt  = 1e-2;                 % [sec] Sampling time
-T   = 2;                    % [sec] Simulation Time
+T   = 1.0;                    % [sec] Simulation Time
 
 k   = floor(T/dt);          % [-]   Sampling indice (last value)
-tControl = 1;               % [sec] Time at which the control is activated
+tControl = 0;               % [sec] Time at which the control is activated
 
 reducedFreq = [0.05:0.05:0.5, 0.6:0.1:1.0, 1.2:0.2:2.0];    % set of reduced frequencies
 gamma       = [0.2, 0.4, 0.6, 0.8];                         % lag parameters
@@ -30,8 +30,6 @@ simcond = struct('k',reducedFreq,'gamma',gamma,'nLAG',length(gamma),'samplingTim
 K = controlDesign(params,fltcond,simcond);
 
 %% SIMULATION
-options_ode45 = odeset('RelTol', 1e-3, 'AbsTol', 1e-6);
-
 x = zeros(15,k+1);      % State Vector (for all sampling time steps)
 input = zeros(1,k+1);   % Control action value (for all sampling time steps)
 x(1:3,1) = x0;
@@ -42,7 +40,7 @@ for i=1:k
     
     % Set control action value
     if i >= tControl/dt
-        input(i) = -15;%K*x(:,i);
+        input(i) = -K*x(:,i);
         % input(i) = min(input(i), 15*pi/180);
         % input(i) = max(input(i), -15*pi/180);
     else
@@ -51,21 +49,9 @@ for i=1:k
 
     % Evolving the system dynamics
     y0 = x(:,i);
-    [t, y] = ode45(@(t,y) system(t,y,sys,input(i)), [0.0 dt], y0);
+    [t, y] = ode45(@(t,y) (sys.A*y+sys.B.*input(i)), [0.0 dt], y0);
     x(:,i+1) = y(length(t),:);
 end
 
 % Plot Results
 plotResults(T,k,x,input);
-
-%% 
-% Dynamic model of the system
-function xdot = system(~,y,sys,input)
-    % Input variables
-    A    = sys.A;
-    B    = sys.B;
-    u    = input;
-
-    % State-Space systems equations
-    xdot = A*y + B.*u;
-end
